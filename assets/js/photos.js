70fb077a1739a230474205b9c7ccd432705d7a09
kazuki-nisshin <nisshin@dtdsh.com>
2018-12-28 03:53:24 +0900

    VR画像の追加

--- assets/js/photos.js
+++ assets/js/photos.js
@@ -1,7 +1,7 @@
 jQuery($ => {
-		let json = 'https://dtdsh.com/wp-json/wp/v2/media?per_page=100&order=asc';
+		let json = 'https://dtdsh.com/wp-json/wp/v2/media?per_page=100&order=asc&orderby=id';
 		let restJSON;
-		for (let i = 1; i < 4; i++) {
+		for (let i = 1; i < 5; i++) {
 				let pJson = json + '&page=' + i;
 				restJSON = $.getJSON(pJson, results => {
 						let showImage, linkImage, item;
@@ -9,10 +9,19 @@ jQuery($ => {
 								if (obj['mime_type'] === 'application/pdf') {
 										return true;
 								} else {
-										showImage = obj['source_url'].replace('https://', 'https://i1.wp.com/') + "?resize=300,300";
-										linkImage = obj['source_url'].replace('https://', 'https://i0.wp.com/') + "?fit=1777,1000";
-										item = `<div class="column"><a class="js--zoom" href="${linkImage}"><img data-src="${showImage}" class="lazyload"></a></div>`;
-										$('#macy--wrap').append(item);
+										if (obj['source_url'].match(/(R\d{7}_\d{14})/g)) {
+												ImageName = obj['source_url'].match(/(R\d{7}_\d{14})/g)[0];
+												showImage = obj['source_url'].replace('https://', 'https://i1.wp.com/') + "?resize=300,300";
+												linkImage = `../vr-view.html?image=${ImageName}`;
+												item = `<div class="column"><a class="link--pano" target="_blank" href="${linkImage}"><img data-src="${showImage}" class="lazyload"></a></div>`;
+												$('#macy--wrap').append(item);
+
+										} else {
+												showImage = obj['source_url'].replace('https://', 'https://i1.wp.com/') + "?resize=300,300";
+												linkImage = obj['source_url'].replace('https://', 'https://i0.wp.com/') + "?fit=1777,1000";
+												item = `<div class="column"><a class="js--zoom" href="${linkImage}"><img data-src="${showImage}" class="lazyload"></a></div>`;
+												$('#macy--wrap').append(item);
+										}
 								}
 						});
 						if (i === 1) {
